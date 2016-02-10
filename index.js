6c8a1978dc2da076b85a5ddeab7622ec46bd1207
Scott O'Malley <scottyomalley@gmail.com>
2016-02-10 13:46:21 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+var KerbalStuffWrapper = function () {};
+
+KerbalStuffWrapper.prototype. = function () {
+  console.log('buz!');
+};
+
+module.exports = new KerbalStuffWrapper();
