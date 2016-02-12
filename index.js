794b571039972ea8fc1230eaae4b39cdc8500c43
Scott O'Malley <scottyomalley@gmail.com>
2016-02-12 01:18:17 +0000

    Implemented the browse endpoints for KerbalStuff.com resolves #1, resolves #2, resolves #3, resolves #4

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ const baseRequest = require('request-promise').defaults({
 function KerbalStuffWrapper(){}
 
 /**
- * @param  {number} [page]
- * @param  {string} [orderBy]
- * @param  {string} [order]
- * @param  {int} [count]
+ * Gets mods sorted by selected conditions from KerbalStuff.com
+ * @param  {number} [page] - Which page of results to retrieve
+ * @param  {string} [orderBy] - Which property of mod use for ordering. Valid values: name, updated, created. Default: created
+ * @param  {string} [order] - Which ordering direction to use. Valid values: asc, desc. Default: asc. [optional]
+ * @param  {int} [count] - Which count of mods to show per page. Valid values: 1-500. Default 30. [optional]
  * @return {Promise}
  */
 
@@ -31,6 +32,59 @@ KerbalStuffWrapper.prototype.browse = function(page, orderBy, order, count) {
 	return baseRequest.get(options);
 };
 
+/**
+ * Gets the newest mods on KerbalStuff.com
+ * @param  {number} page - Which page of results to retrieve [optional]
+ * @return {Promise}      
+ */
+KerbalStuffWrapper.prototype.browseNew = function(page) {
+
+	let options = {
+		uri: '/browse/new',
+		qs: {
+			page: page			
+		}
+	};
+
+	return baseRequest.get(options);
+};
+
+/**
+ * Gets the top mods on KerbalStuff.com
+ * @param  {number} page - Which page of results to retrieve [optional]
+ * @return {Promise}      
+ */
+KerbalStuffWrapper.prototype.browseTop = function(page) {
+
+	let options = {
+		uri: '/browse/top',
+		qs: {
+			page: page			
+		}
+	};
+
+	return baseRequest.get(options);
+};
+
+/**
+ * Gets the featured mods on KerbalStuff.com
+ * @param  {number} page - Which page of results to retrieve [optional]
+ * @return {Promise}      
+ */
+KerbalStuffWrapper.prototype.browseFeatured = function(page) {
+
+	let options = {
+		uri: '/browse/featured',
+		qs: {
+			page: page			
+		}
+	};
+
+	return baseRequest.get(options);
+};
+
+
+
 KerbalStuffWrapper.prototype.search = function(searchString) {
 
 	let options = {
@@ -43,10 +97,9 @@ KerbalStuffWrapper.prototype.search = function(searchString) {
 	return baseRequest.get(options);
 };
 
-var test = new KerbalStuffWrapper();
-test.browse()
-    .then(json => console.log(json))
-    .catch(error => console.log(error));
+
+
+
 
 
 
